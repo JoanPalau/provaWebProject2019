1e8ce9e33957ea5b0f7ad8a34d835eae5ad3255f
Joan Palau <joanp4l4u@gmail.com>
2019-02-26 09:26:05 +0100

    Implementat correctament client per obtenir el temps

--- weatherclientapi.py
+++ weatherclientapi.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
-import bs4
+import json
+import pprint
 
 class ClientWeb(object):
-    """"Client web per la web de la EPS"""
+    """"Client web per obtenir la temperatura de Lleida"""
     def __init__(self):
         super(ClientWeb, self).__init__()
         pass
@@ -10,25 +11,25 @@ class ClientWeb(object):
     def run(self):
         # descaregar-me html
         data = self.do_request()
-        print(data)
+        # print(data)
         # buscar activitats
         data = self.process_weather(data)
         # imprimir resultat
-
+        print(data)
 
     def do_request(self):
-
-        f = urlopen("http://api.openweathermap.org/data/2.5/find?q=London&units=metric&APPID=eceb0dc81e2deb91fc62eb578ecd0180")
+        f = urlopen("http://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&APPID=eceb0dc81e2deb91fc62eb578ecd0180&mode=json&lang=ca")
         data = f.read()
         f.close()
         return data
 
     def process_weather(self, html):
-        arbre = bs4.BeautifulSoup(html, features="lxml")
-        temperature = arbre.find("main")
-        weather = arbre.find("weather")
-        print(temperature["temp"] + " and " + weather["description"])
-        return None
+        dic = json.loads(html)
+        # pprint.pprint(dic)
+        temp = dic['list'][0]['main']['temp']
+        weather = dic['list'][0]['weather'][0]['description']
+        return (str(temp)+" and "+weather)
+
 
 if __name__ == "__main__":
     c = ClientWeb()
